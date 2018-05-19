b6359eb7d3c3883683f5ac57ddd8fd4a2248b263
Norbert <norbertkozsir@gmail.com>
2018-05-19 22:45:57 +0200

    info screen

--- lib/info_page.dart
+++ lib/info_page.dart
@@ -14,9 +14,32 @@ class InfoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(),
+      appBar: new AppBar(
+        title: new Text("Info"),
+      ),
       body: new Column(
         children: <Widget>[
+          new ExpansionTile(
+            title: new Text("Sources"),
+            children: <Widget>[
+              new ListTile(
+                title: new Text("https://www.reddit.com/r/AppIdeas/"),
+                onTap: ()=>_open("https://www.reddit.com/r/AppIdeas/"),
+              ),
+              new ListTile(
+                title: new Text("https://www.reddit.com/r/Startup_Ideas/"),
+                onTap: ()=>_open("https://www.reddit.com/r/Startup_Ideas/"),
+              ),
+              new ListTile(
+                title: new Text("https://www.reddit.com/r/Lightbulb/"),
+                onTap: ()=>_open("https://www.reddit.com/r/Lightbulb/"),
+              ),
+              new ListTile(
+                title: new Text("https://www.reddit.com/r/CrazyIdeas/"),
+                onTap: ()=>_open("https://www.reddit.com/r/CrazyIdeas/"),
+              ),
+            ],
+          ),
           new ListTile(
             title: new Text("Do you like this app? Let me know and rate it on Google Play"),
             leading: new Icon(Icons.stars),
